826cc90b6c9dfbaabede1c7e357186a7f53d32c2
Juudini <juudinidev@gmail.com>
2024-03-26 14:56:02 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { NextUIProvider } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <NextUIProvider>
+      <h1>Hellou world ! :P </h1>
+    </NextUIProvider>
+  );
+}
